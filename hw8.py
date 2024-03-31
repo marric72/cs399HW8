1d5fba9752967cedc80f68f1cfce8be1bc674d69
Heather Marriott <heathermarriott1@gmail.com>
2024-03-31 14:22:42 -0700

    roses

--- hw8.py
+++ hw8.py
@@ -14,12 +14,12 @@ st.set_page_config(
 #Write text to the screen with HTML markup
 title = '<p style="font-family:Courier; color:Blue; font-size: 30px;">CS399 AI Example - Find Outliers</p>'
 st.markdown(title, unsafe_allow_html=True)
-#st.image("panda.png", caption='Similarity Checking Panda Running on AI')
+st.image("yellowRose.png", caption='Relax')
 title = '<p style="font-family:Courier; color:White; font-size: 20px;">Based on Pre-trained vectors using Wikipedia 2017 </p>'
 st.markdown(title, unsafe_allow_html=True)
 
 st.write("This can take awhile, so please enjoy the zen garden while you wait.")
-
+st.image("redRose.png", caption='Breath') 
 # Display the filtered DataFrame
 st.title("Outlier Removal")
 st.write("Enter at least 3 comma-seperated words")
