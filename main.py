632ae35230975fc7ba88c54e2fb6a9879aedadde
Heather Marriott <heathermarriott1@gmail.com>
2024-04-02 21:54:22 -0700

    passes tests, ignore spaces

--- main.py
+++ main.py
@@ -8,20 +8,44 @@ model = Model("models/glove_short.txt") #model selected because it is smallest f
 while True: 
     line=input('Enter at least 3 comma seperated words: ')
     words=line.split(',')
-    scores=[]
+    #print(f"words={words}")
     if len(words) < 3:
-        print("Error: you did not enter 3 words with commas in between.")
+        print("Error: you did not enter 3 words with commas in between. Try again.g")
         continue
-    for word in words:
-        W = model.find_word(word)
-        if W == None:
-            print(f"Word not found in model: {word}")
-            import sys
-            sys.exit(1) #exit program, a break would only exit for loop
-        scores.append(W.norm()) #TODO: how can I use similarity ???
-    z_scores = zscore(scores)
+
+    scores=[]
+    for x in range(0, len(words)):
+        scoreTotal=0
+        for y in range(0, len(words)):
+            if x==y:
+                continue
+            #print(f"x={x} y={y}")
+            word1 = model.find_word(words[x])
+            word2 = model.find_word(words[y])
+            if word1 == None :
+                print(f"Word not found in model: ***{words[x]}***")
+                import sys
+                sys.exit(1) #exit program, a break would only exit for loop
+            if word2 == None:
+                print(f"Word not found in model: ***{words[y]}***")
+                import sys
+                sys.exit(1) #exit program, a break would only exit for loop
+            print(f"word1={word1} word2={word2}")
+            if word1 != None and word2 != None:
+                #print(f"calling similarity for {words[x]} and {words[y]}")
+                s=word1.similarity(word2)
+                #print(f"calling similarity for {words[x]} and {words[y]} s={s}")
+                scoreTotal=scoreTotal + s
+        scores.append(scoreTotal)
+    print(f"scores={scores}")
+    scores=zscore(scores)
+    print(f"zscores={scores}")
+    
+    
+    #Todo: use z_score somehow  z_scores = zscore(scores)
+    #print(f"z_scores={z_scores}")
     threshold = 1
-    outliers_indices = np.where(np.abs(z_scores) > threshold)[0]
+    outliers_indices = np.where(np.abs(scores) >threshold)[0]
     wordCopy=words.copy()
     for o in outliers_indices:
         words.remove(wordCopy[o])  #remove outliers
