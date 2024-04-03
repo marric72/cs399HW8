08c82a7d01c1f5792b51f6aa7ba0e96a95bef244
Heather <71154676+marric72@users.noreply.github.com>
2024-04-03 10:06:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,20 @@
 # so I removed Cat.  Removed non-word lines example: , and .
 # to get under the GitHub file limits so I did not need Large File Storage.
 #
-#to-do: smaller model file
+#  Lines of Code: 41 - 6(blank lines) = 37
+#  Lines of Comments:
+#to-do: 
 #docstrings and comments
 #stop at stop
 # less than 20 lines
 # update streamlit app
 #check pycharm for warnings
+
 from wv import Model
 from scipy.stats import zscore
 import numpy as np
 
 model = Model("models/glove_shorter.txt") 
-#model = Model("models/outfileWiki.txt")
 line=input('Enter at least 3 comma seperated words: (STOP to exit)')
 while line != 'STOP': 
     line=line.replace(" ","")
@@ -33,37 +35,26 @@ while line != 'STOP':
         break
     
     if len(words) < 3:
-        print("Error: you did not enter 3 words with commas in between. Try again.g")
+        print("Error: you did not enter 3 words with commas in between. Try again.")
         continue
 
     scores=[]
     for x in range(0, len(words)):
         scoreTotal=0
         for y in range(0, len(words)):
-            if x==y:
-                continue
-            #print(f"x={x} y={y}")
+            if x == y:
+                continue # do not compare word with itself
             word1 = model.find_word(words[x])
             word2 = model.find_word(words[y])
-            
-            print(f"word1={word1} word2={word2}")
-            if word1 != None and word2 != None:
-                #print(f"calling similarity for {words[x]} and {words[y]}")
-                s=word1.similarity(word2)
-                #print(f"calling similarity for {words[x]} and {words[y]} s={s}")
-                scoreTotal=scoreTotal + s
+
+            scoreTotal += word1.similarity(word2)
         scores.append(scoreTotal)
-    print(f"scores={scores}")
-    scores=zscore(scores)
-    print(f"zscores={scores}")
+    scores = zscore(scores)
     
-    
-    #Todo: use z_score somehow  z_scores = zscore(scores)
-    #print(f"z_scores={z_scores}")
     threshold = 1
     outliers_indices = np.where(np.abs(scores) >threshold)[0]
-    wordCopy=words.copy()
+    wordCopy = words.copy()
     for o in outliers_indices:
         words.remove(wordCopy[o])  #remove outliers
     print('With outliers removed, your list looks like this:', ", ".join(words))   
-    line=input('Enter at least 3 comma seperated words: (STOP to exit)') 
+    line = input('Enter at least 3 comma seperated words: (STOP to exit)')
