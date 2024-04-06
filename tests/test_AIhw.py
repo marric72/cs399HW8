3f94ab474d3529ee7c272b1deae5a1a50076a5d9
Heather Marriott <heathermarriott1@gmail.com>
2024-04-06 08:56:25 -0700

    changes

--- tests/test_AIhw.py
+++ tests/test_AIhw.py
@@ -2,18 +2,21 @@ import cs399_HW8Marriott
 import pytest
 
 def test_test1():
+	"""from HWK assignment"""
 	words=['apple', 'banana', 'mango', 'car']
 	model = cs399_HW8Marriott.Load_Model()
 	new_list = cs399_HW8Marriott.Sim_Check(model, words)
 	assert new_list == ['apple', 'banana', 'mango']
 
 def test_test2():
+	"""from HWK assignment"""
 	words=['apple', 'banana', 'mango', 'car', 'pizza']
 	model = cs399_HW8Marriott.Load_Model()
 	new_list = cs399_HW8Marriott.Sim_Check(model, words)
 	assert new_list == ['apple', 'banana', 'mango', 'pizza']
 
 def test_test3():
+	"""from HWK assignment"""
 	words=['apple', 'banana', 'mango', 'orange', 'car', 'bus', 'cherry']
 	model = cs399_HW8Marriott.Load_Model()
 	new_list = cs399_HW8Marriott.Sim_Check(model, words)
@@ -25,3 +28,10 @@ def test_all_same():
 	model = cs399_HW8Marriott.Load_Model()
 	new_list = cs399_HW8Marriott.Sim_Check(model, words)
 	assert new_list == words
+
+def test_word_not_found():
+	"""nonsense words that will not be in model"""
+	words=['aksljdjka','cccaaaaa','cattttt']
+	model = cs399_HW8Marriott.Load_Model()
+	new_list = cs399_HW8Marriott.Sim_Check(model, words)
+	assert new_list == None
